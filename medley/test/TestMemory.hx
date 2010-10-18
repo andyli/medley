e960f856f949040d52cc6cab04f8be077ea877a6
Andy Li <andy@onthewings.net>
2010-10-19 02:45:02 +0800

    updated TestMemory

--- medley/test/TestMemory.hx
+++ medley/test/TestMemory.hx
@@ -2,7 +2,7 @@ package medley.test;
 
 import flash.display.Sprite;
 import flash.geom.Point;
-import flash.system.System;
+import flash.utils.TypedDictionary;
 import flash.Lib;
 import hsl.haxe.Signal;
 import utest.Assert;
@@ -16,24 +16,11 @@ class TestMemory {
 	public function new(){}
 
 	public function testSimpleMedley():Void {
-		var oUsage = Math.NaN;
-		var assert = Assert.createAsync(function(){
-			System.gc();
-			Assert.floatEquals(oUsage, System.totalMemory, oUsage * 0.2);
-		}, 6000);
-		
-		runSimpleMedley(5000);
-		haxe.Timer.delay(function(){
-			System.gc();
-			oUsage = System.totalMemory;
-			runSimpleMedley(5000);
-			haxe.Timer.delay(assert, 1200);
-		}, 1200);
-	}
+		var trash = new Trash();
 
-	static function runSimpleMedley(num:Int):Void {
+		var numOfSp = 5000;
 		var init = new Point(Lib.current.stage.stageWidth*0.5, Lib.current.stage.stageHeight*0.5);
-		for (i in 0...num) {
+		for (i in 0...numOfSp) {
 			var sp = new Sprite();
 			sp.graphics.beginFill(Std.int(Math.random()*0xFFFFFF));
 			sp.graphics.drawCircle(0,0,4);
@@ -51,12 +38,79 @@ class TestMemory {
 			
 			var onEnd = function() {
 				sp.parent.removeChild(sp);
+				trash.throws(sp);
+				trash.throws(m);
+				trash.throws(m.events);
 				//m.destroy();
+				if (--numOfSp == 0) { //if all Medley are ended.
+					Trash.collectAll();
+				}
 			};
 			
 			m.events.tick.bind(onTick);
 			m.events.reachEnd.bindVoid(onEnd);
 			m.play();
 		}
+		
+		var assert = Assert.createAsync(function(){
+			Assert.isTrue(trash.isEmpty());
+			Assert.equals(0,trash.garbages().count());
+		}, 2000);
+
+		haxe.Timer.delay(assert,1100);
 	}
+}
+
+//http://gist.github.com/632717
+#if flash9
+import flash.utils.TypedDictionary;
+import flash.system.System;
+import flash.system.Capabilities;
+#else
+#error
+#end
+
+using Lambda;
+
+/*
+	A Trash that you can throw unused objects inside and see if they are GC'ed later.
+	http://blog.onthewings.net/2010/10/18/how-to-know-objects-are-really-gced-in-flash-as3/
+*/
+class Trash {
+	public function new():Void {
+		dict = new TypedDictionary<Dynamic,String>(true);
+	}
+
+	/*
+		Throws a garbage, an unused object that should be GC'ed soon, to the Trash.
+		An optional identifier that let you know what is in the Trash by garbages().
+	*/
+	public function throws(garbage:Dynamic, ?identifier:String):Void {
+		dict.set(garbage, identifier == null ? Std.string(garbage) : identifier);
+	}
+
+	/*
+		Return an Array of identifier of the garbages inside.
+		It does not let you pick out the garbages, since they are dirty, and you shouldn't hold reference of them again.
+	*/
+	public function garbages():Array<String> {
+		return dict.array();
+	}
+
+	/*
+		Tell if the Trash is empty.
+	*/
+	public function isEmpty():Bool {
+		return dict.empty();
+	}
+
+	/*
+		Trigger GC. Throws an error if it is not running in a debugger.
+	*/
+	static public function collectAll():Void {
+		if (!Capabilities.isDebugger) throw "System.gc() only works on flash debugger.";
+		System.gc();
+	}
+
+	var dict:TypedDictionary<Dynamic,String>;
 }
